b641fbadee95dca84c81627b8cb4bfa44e4175ca
Alexa <akaminsky9@gmail.com>
2014-08-30 23:45:50 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TvSchedule::Application.config.secret_token = 'a18da45c37e478046abffd4adb0c237a134c5d529df803780b09a9d7013bcd0052eba8977efa64a4001449cb3628f68df3f38002926923fff8a009ea59e7d8dc'
